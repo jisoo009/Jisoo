28ecb05720f9d652da5a5e6022732d56c5ce5616
choihanni27 <choihanni27@gmail.com>
2026-04-15 12:10:06 +0900

    배열 코딩테스트

---
+++ LinkedList.mjs
@@ -0,0 +1,93 @@
+class Node {
+    constructor(data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.count = 0;
+    }
+
+    printAll() {
+        let currentNode = this.head;
+        let text = "[";
+        while (currentNode != null) {
+           // console.log(currentNode.data);
+            text += currentNode.data;
+            currentNode = currentNode.next;
+            if (currentNode != null) text += ","
+        }
+        text += "]";
+        console.log(text);
+    }
+    clear() {
+        this.head = null;
+        this.count = 0;
+    }
+
+    insertAt(index, data) {
+        if (index < 0 || index > this.count) {
+            throw new Error("범위를 벗어났습니다.");
+        }
+        let newNode = new Node(data);
+        if (index === 0) {
+            newNode.next = this.head;
+            this.head = newNode;
+        } else {
+            let currentNode = this.head;
+            for (let i = 0; i < index - 1; i++) {
+                currentNode = currentNode.next;
+            } newNode.next = currentNode.next;
+            currentNode.next = newNode;
+        }
+        this.count++;
+    }
+    insertLast(data){
+        this.insertAt(this.count,data);
+    }
+    
+    deleteAt(index) {
+        if (index < 0 || index >= this.count) {
+            throw new Error("범위를 벗어났습니다.");
+        }
+
+        let currentNode =this.head;
+        if(index === 0){
+            let deleteNode = this.head;
+            this.head = this.head.next;
+            this.count--;
+            return deleteNode;
+        }
+        else{
+            for (let i = 0; i < index-1; i++){
+                currentNode = currentNode.next;
+            }
+            let deleteNode = currentNode.next;
+            currentNode.next = currentNode.next.next;
+            this.count--;
+            return deleteNode;
+        }
+}
+
+deleteLast(){
+   // this.deleteAt(4);
+    this.deleteAt(this.count-1);
+}
+
+getNodeAt(index){
+    if (index < 0 || index > this.count) {
+            throw new Error("범위를 벗어났습니다.");
+        }
+        let currentNode = this.head;
+        for (let i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        } 
+        return this.getNodeAt;
+
+    }
+
+}
+export { Node, LinkedList };
--- test.mjs
+++ test.mjs
@@ -1,2 +1,52 @@
-let str = "헬로~~ 자바스크립트";
-console.log(str);
+import { Node, LinkedList } from "./LinkedList.mjs";
+
+// let node1 = new Node(1);
+// let node2 = new Node(2);
+// let node3 = new Node(3);
+
+// node1.next = node2;
+// node2.next = node3;
+
+// console.log(node1.data);
+// console.log(node1.next.data);
+// console.log(node1.next.next.data);
+
+
+let list = new LinkedList();
+console.log("===== insertAt() =====");
+list.insertAt(0,0);
+list.insertAt(1,1);
+list.insertAt(2,2);
+list.insertAt(3,3);
+list.insertAt(4,4);
+//list.insertAt(3,99);
+list.printAll();
+
+// console.log("");
+// console.log("===== insertAt() =====");
+// list.clear();
+// list.printAll();
+
+// console.log("");
+// console.log("===== insertAt() =====");
+// list.insertLast(99);    //list.insertAt(5,99);
+// list.insertLast(100);
+// list.insertLast(101);
+// list.printAll();
+
+
+// console.log("");
+// console.log("===== deleteAt() =====");
+// list.deleteAt(0);
+// list.deleteAt(3);
+// list.printAll();
+
+console.log("");
+console.log("===== deleteLast() =====");
+list.deleteLast();
+list.printAll();
+
+console.log("");
+console.log("===== getNodeAt() =====");
+let secondnNode=list.getNodeAt(0);
+console.log(secondnNode);
